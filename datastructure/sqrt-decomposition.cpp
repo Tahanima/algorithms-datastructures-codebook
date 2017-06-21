29d84b759c21af3b00a8b5d175575ad26a654d23
Tahanima Chowdhury <tahanimachowdhury@gmail.com>
2017-06-22 01:31:55 +0600

    Added a file

--- datastructure/sqrt-decomposition.cpp
+++ datastructure/sqrt-decomposition.cpp
@@ -41,13 +41,13 @@ int query(int left, int right){
 int main(){
 	build();
 
-	printf("Summation of numbers from index %d and %d is: %d\n", 1, 9, query(1, 9));
-	printf("Summation of numbers from index %d and %d is: %d\n", 3, 5, query(3, 5));
-	printf("Summation of numbers from index %d and %d is: %d\n", 2, 7, query(2, 7));
-	printf("Summation of numbers from index %d and %d is: %d\n", 5, 5, query(5, 5));
+	printf("Summation of numbers from index %d to %d is: %d\n", 1, 9, query(1, 9));
+	printf("Summation of numbers from index %d to %d is: %d\n", 3, 5, query(3, 5));
+	printf("Summation of numbers from index %d to %d is: %d\n", 2, 7, query(2, 7));
+	printf("Summation of numbers from index %d to %d is: %d\n", 5, 5, query(5, 5));
 	update(0, 9);
-	printf("Summation of numbers from index %d and %d is: %d\n", 0, 9, query(0, 9));
+	printf("Summation of numbers from index %d to %d is: %d\n", 0, 9, query(0, 9));
 	update(3, 10);
-	printf("Summation of numbers from index %d and %d is: %d\n", 1, 8, query(1, 8));
+	printf("Summation of numbers from index %d to %d is: %d\n", 1, 8, query(1, 8));
 	return 0;
 }
